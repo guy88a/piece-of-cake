24b577d4ba434a848a7981a5a6c35a050e1e2d1a
unknown <guy88prog@gmail.com>
2020-07-20 21:25:44 +0300

    <Code> Core | Update | New Files: config, context, dom and scene utils

--- engine/core/core.js
+++ engine/core/core.js
@@ -2,11 +2,22 @@
 ============================== CORE =================================
 ---------------------------------------------------------------------
 Main Object (POG & pog) Initiator
-Version: 0.01.000
+Version: 0.01.100
 ---------------------------------------------------------------------
 =====================================================================
 */
 
+/* IMPORTS ==========================================================
+    - Initiators
+=====================================================================
+*/
+import DomUtil from './u_dom.js';
+
+/* SETTINGS =========================================================
+    - Main variables
+    - Default variables
+=====================================================================
+*/
 let defaultPogObject = {
     DOM: {},
     Scene: {},
@@ -14,4 +25,35 @@ let defaultPogObject = {
     Configurations: {}
 }
 
-var POG = POG || pog || defaultPogObject;
+var POG = POG || pog || InitiateCore();
+
+/* FUNCTION =========================================================
+    - Core initiator
+=====================================================================
+*/
+/**
+ * InitiateCore
+ * construct the main game object - POG
+ */
+function InitiateCore() {
+    // user try for handling dom exception
+    // if fails, returns a default game object
+    try {
+        // get all main inner objects
+        let dom_util     = DomUtil();
+        let scene_util   = {};
+        let context_util = {};
+        let config_util  = {};
+    
+        // return constructed game object
+        return {
+            DOM: dom_util,
+            Scene: scene_util,
+            Context: context_util,
+            Configurations: config_util,
+        }
+    } catch(e) {
+        // return default game object
+        return defaultPogObject;
+    }
+}
